ca0bd1ac74cd9f2b0c1b976e8351d33028676eca
Emre Aslan <emreaslan7@icloud.com>
2022-10-02 00:44:32 +0300

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header(){
   return (
     <div className="flex justify-between items-center border-[#F7931A] border-2 p-5">
         <img className="h-8" alt="" src={bitcoin}/>
-        <h1 className="text-4xl text-[#F7931A] font-bold">Spend 1 Bitcoin</h1>
+        <h1 className="text-4xl text-[#F7931A] font-extrabold">Spend 1 Bitcoin</h1>
 
 
     </div>
